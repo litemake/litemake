e7d51c8b20a75b3680565b9f5702294a15da6531
Alon (Ubuntu) <downtown2u@gmail.com>
2021-08-31 16:58:43 +0300

    👷 Added `setup.py` script

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+from nanomake import __version__, __description__
+
+with open('README.md', mode='r', encoding='utf8') as f:
+    README = f.read()
+
+
+with open('requirements.txt', mode='r', encoding='utf8') as f:
+    DEPENDENCIES = f.read().splitlines()
+
+setup(
+    name="nanomake",
+    version=__version__,
+    description=__description__,
+    python_requires='>=3.9,<4',
+
+    long_description=README,
+    long_description_content_type='text/markdown',
+
+    author='Alon Krymgand Osovsky',
+    author_email='downtown2u@gmail.com',
+
+    packages=['nanomake'],
+    install_requires=DEPENDENCIES,
+)
