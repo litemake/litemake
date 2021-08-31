25cc692893f34aaaa02f717d03e4867f07748e61
Alon (Ubuntu) <downtown2u@gmail.com>
2021-08-31 16:49:50 +0300

    🎨 Added `nanomake/__init__.py`

---
+++ nanomake/__init__.py
@@ -0,0 +1,11 @@
+''' nanomake - Alon Krymgand Osovsky (2021) '''
+
+__version__ = '0.1.0'
+__description__ = 'A new way to build, test and distribute your C/C++ projects and libraries.'
+__nanomake_spec__ = 1
+
+__all__ = [
+    'NanomakePrinter',
+]
+
+from .printer import NanomakePrinter
