3d8c1dad753de96028c18bf68de1a095ec97ea31
Alon (Ubuntu) <downtown2u@gmail.com>
2021-09-15 22:56:12 +0300

    ✨ Added the `COMPILERS` dict

--- litemake/compile/compilers/__init__.py
+++ litemake/compile/compilers/__init__.py
@@ -1,3 +1,15 @@
 from .base import AbstractCompiler
 from .gnu import GccCompiler, GplusplusCompiler
 from .llvm import ClangCompiler
+
+COMPILERS = {
+    compiler.name(): compiler
+    for compiler in
+    {GccCompiler, GplusplusCompiler, ClangCompiler}
+}
+
+__all__ = [
+    'AbstractCompiler', 'COMPILERS',
+    'GccCompiler', 'GplusplusCompiler',
+    'ClangCompiler',
+]
