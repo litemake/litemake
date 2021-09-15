ce76100b62363cb39e7c00e829675dc51b1eb587
Alon (Ubuntu) <downtown2u@gmail.com>
2021-09-16 01:10:52 +0300

    ⚡️ Changed test virtual project directory name

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,7 @@ from tests.utils import VirtualProject
 
 @pytest.fixture
 def project(request, tmpdir_factory):
-    name = request.node.originalname
+    name = request.node.nodeid.replace('/', ':')
     return VirtualProject(
         name=name,
         basepath=str(tmpdir_factory.mktemp(name)),
