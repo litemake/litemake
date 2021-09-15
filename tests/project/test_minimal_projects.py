3023d39275944da2c42d21cc42a3cecc36d13d65
Alon (Ubuntu) <downtown2u@gmail.com>
2021-09-15 03:58:29 +0300

    ✅ Added tests for missing fields in setup file

--- tests/project/test_minimal_projects.py
+++ tests/project/test_minimal_projects.py
@@ -54,3 +54,53 @@ def test_missing_name(project: VirtualProject):
 
     assert err.value.fieldpath == ['litemake', 'meta', 'name']
     assert matching_msg('missing required field', err.value.raw_msg)
+
+
+def test_missing_standard(project: VirtualProject):
+    """ Tests if an appropriate error message is provided when the setup file
+    is missing the 'litemake.meta.standard' field. """
+
+    project.add_setup(
+        '''
+        [litemake.meta]
+        name="testing"
+
+        [target.build]
+        sources=["src/**/*.cpp"]
+        '''
+    )
+
+    with pytest.raises(exceptions.litemakeConfigError) as err:
+        project.make()
+
+    assert err.value.fieldpath == ['litemake', 'meta', 'standard']
+    assert matching_msg('missing required field', err.value.raw_msg)
+
+
+@pytest.mark.parametrize('setup', (
+    # No target entry at all.
+    '''
+    [litemake.meta]
+    name="testing"
+    standard="c++17"
+    ''',
+    # Target entry specified, but no specific target
+    '''
+    [litemake.meta]
+    name="testing"
+    standard="c++17"
+    [target]
+    ''',
+))
+def test_missing_target(project: VirtualProject, setup: str):
+    """ Tests if an appropriate error message is provided when the setup file
+    has no targets specified. """
+
+    project.add_setup(setup)
+
+    with pytest.raises(exceptions.litemakeConfigError) as err:
+        project.make()
+
+    # TODO: a special error message in the case of no targets provided
+    assert err.value.fieldpath == ['target']
+    assert matching_msg('min 1 item(s) required', err.value.raw_msg)
