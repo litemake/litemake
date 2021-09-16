2e6afa259c496f1de8e9161a31990e1373af5b32
Alon (Ubuntu) <downtown2u@gmail.com>
2021-09-16 14:57:04 +0300

    ⚡️ Separated compiler tests into two files

--- tests/compilers/test_compilers.py
+++ tests/compilers/test_compilers.py
@@ -1,16 +1,12 @@
 import os
-import subprocess
-from shutil import which
-from abc import ABC, abstractmethod
 
 import typing
 if typing.TYPE_CHECKING:
     from tests.utils import VirtualProject
 
-import pytest
+from .base import _TestCompiler, skip_if_missing_clis
 
 from litemake.compile.compilers import (
-    AbstractCompiler,
     Compiler,
     ClangCompiler,
     ClangplusplusCompiler,
@@ -19,22 +15,7 @@ from litemake.compile.compilers import (
 )
 
 
-class _TestCompilerC(ABC):
-
-    @property
-    @classmethod
-    @abstractmethod
-    def COMPILER(cls) -> AbstractCompiler:
-        """ Returns an instance to the compiler that is currently being
-        tested. This property should be overwritten using inheritance. """
-
-    @staticmethod
-    def _execute_binary(*cmd) -> typing.Tuple[int, str]:
-        """ Executes the given binary file and returns the return code as an
-        interger and the captured stdout stream, as a string. """
-
-        result = subprocess.run(*cmd, capture_output=True, text=True)
-        return result.returncode, result.stdout
+class _TestCompilerC(_TestCompiler):
 
     def test_c_hello_world(self, project: 'VirtualProject'):
         """ A simple test that creates a file named 'main.c', and tries to
@@ -60,9 +41,8 @@ class _TestCompilerC(ABC):
         out_dest = os.path.join(project.basepath, 'main.out')
         self.COMPILER.create_executable(out_dest, [obj_dest])
 
-        code, out = self._execute_binary(out_dest)
-        assert code == 0, "Compiled program crashed unexpectedly"
-        assert out == 'Hello from litemake!\n', "Unexpected output from compiled program"
+        out = self._execute_binary(out_dest)
+        assert out == 'Hello from litemake!\n'
 
 
 class _TestCompilerCPP(_TestCompilerC):
@@ -86,30 +66,8 @@ class _TestCompilerCPP(_TestCompilerC):
         out_dest = os.path.join(project.basepath, 'main.out')
         self.COMPILER.create_executable(out_dest, [obj_dest])
 
-        code, out = self._execute_binary(out_dest)
-        assert code == 0, "Compiled program crashed unexpectedly"
-        assert out == 'Hello from litemake!\n', "Unexpected output from compiled program"
-
-
-def required_clis_exists(compiler: AbstractCompiler) -> bool:
-    """ Returns `True` only if the given compiler can and compile on the
-    current machine. """
-    clis = compiler.required_clis
-    return all(which(cli) is not None for cli in clis)
-
-
-def skip_if_missing_clis(compiler: AbstractCompiler):
-    """ A simple decorator that uses the 'pytest.mark.skipif' decorator only
-    if the given compiler can't run on this machine because there are required
-    command line exectutable tools that aren't avaliable. """
-
-    def decorator(func):
-        dec = pytest.mark.skipif(
-            not required_clis_exists(ClangplusplusCompiler),
-            reason="Missing required CLIs",
-        )
-        return dec(func)
-    return decorator
+        out = self._execute_binary(out_dest)
+        assert out == 'Hello from litemake!\n'
 
 
 @skip_if_missing_clis(GccCompiler)
